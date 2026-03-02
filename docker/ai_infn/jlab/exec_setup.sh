9bbcbe06c4ffa8bf8e436ffa9722364db8b41a63
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2026-03-02 18:53:00 +0100

    setup environment in initialization

---
+++ docker/ai_infn/jlab/exec_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Retrieve username from env var
+NB_USER="${NB_USER}"
+
+SETUP_SCRIPT=${SETUP_SCRIPT:-"/envs/setup.sh"} 
+LOG_FILE="/var/logs/envs-setup.log"
+
+if [ -z "$NB_USER" ]; then
+	echo "NB_USER environment variable not set."
+	exit 0
+fi
+
+
+if [ -f "$SETUP_SCRIPT" ]; then
+	# Run setup script as NB_USER in background with 5 min timeout
+	(
+		sudo -u "$NB_USER" timeout 300 bash "$SETUP_SCRIPT" 2>&1 | sudo tee "$LOG_FILE" > /dev/null
+	) &
+else
+	echo "$SETUP_SCRIPT does not exist."
+fi
+
