5496aa26422e610eecce3109724e654b1f05559f
Lucio Anderlini <Lucio.Anderlini@fi.infn.it>
2026-04-09 15:27:43 +0200

    install proot

---
+++ docker/ai_infn/jlab/proot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script is a wrapper to execute apptainer sandboxes using proot, allowing to run them without root privileges.
+
+./proot \
+    --bind=/dev \
+    --bind=/var/tmp \
+    --bind=/home \
+    --bind=/etc/passwd \
+    --bind=/etc/group \
+    --bind=/etc/shadow \
+    --bind=/etc/sudoers \
+    --bind=/cvmfs \
+    --bind=/proc \
+    "$@"
