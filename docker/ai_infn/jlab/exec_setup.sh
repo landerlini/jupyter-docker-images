c496130cb82f27445cf750fc78141ed45fd04e4f
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2026-09-20 16:16:50 +0200

    fixed condition

--- docker/ai_infn/jlab/exec_setup.sh
+++ docker/ai_infn/jlab/exec_setup.sh
@@ -16,7 +16,7 @@ if [ -e "/mnt/slurm/slurm.key"]; then
 	cp /mnt/slurm/slurm.key /etc/slurm
 	chown slurm /etc/slurm/slurm.key
 	chmod 400 /etc/slurm/slurm.key
-	if [ -e "/mnt/slurm/slurm.key"]; then
+	if [ -e "/slurm/sbin/sackd"]; then
 		(
 			sudo -u slurm /slurm/sbin/sackd -D $SACKD_ARGS | tee /var/log/slurm-sackd.log 
 		) &
